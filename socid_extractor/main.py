bbe44cf6fcd5080651469bd97846d5fcfc182d77
cyberkotik <70950106+cyb3rk0tik@users.noreply.github.com>
2020-09-14 15:04:01 +0300

    Update main.py

--- socid_extractor/main.py
+++ socid_extractor/main.py
@@ -8,8 +8,6 @@ from http.cookies import SimpleCookie
 import requests
 
 from .utils import *
-from schemes import schemes
-
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
